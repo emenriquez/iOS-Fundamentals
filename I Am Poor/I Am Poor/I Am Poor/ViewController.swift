53e30f1994805cf6d5a3e43cc71ec81b01175bbe
Erik Enriquez <Erik@Eriks-MacBook-Air.local>
2019-10-16 09:51:24 -0500

    initial commit

---
+++ I Am Poor/I Am Poor/I Am Poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Erik Enriquez on 10/15/19.
+//  Copyright © 2019 Erik Enriquez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
